96a2a74ca6fe86e3d41a94ee235cc6af03e39442
Gaji Yaqub <gajiyakub6@gmail.com>
2025-04-16 17:27:39 -0700

    Worked on Manipulation of forms and submitting to the DB

--- HISC/baseapp/forms.py
+++ HISC/baseapp/forms.py
@@ -8,6 +8,24 @@ class Contactform(forms.Form): # This is a python class
 
 
 class ContactModelForm(forms.ModelForm):
+  name = forms.CharField(label="Enter your name", 
+                         help_text= "Kindly input your name in Capital Letter",
+                         widget=forms.TextInput(attrs={
+                        'class': 'form-control bg-light' # This is a attribute that can have a attribute used to carefully design the forms.
+                        
+  }))
+  email = forms.EmailField(label="Enter your email", widget=forms.EmailInput(attrs={
+    'class': 'form-control b-light'
+  }))
+  mobile = forms.IntegerField(label="Enter you mobile number", widget=forms.TextInput(attrs={
+    'class': 'form-control bg-light'
+  }))
+
+  message = forms.CharField(label="Your message", widget=forms.Textarea(attrs={
+    'class': 'form-control bg-class'
+  }))
+
+  
   class Meta:
     model = ContactM # To avoid Creating the fields over again
     fields = '__all__' # or ['name', 'email'] you decide the fields you want to display                                                                                                                 
